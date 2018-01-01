9fd89d28287ebf2ca91f8f3972755f8ac58ddffb
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-01 11:19:58 +0700

    Imporve performance by modal should rerender only if it shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliarys';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }} 
-    >
-      {props.children}
-    </div>
-    <Backdrop 
-      show={props.show}
-      clicked={props.modalClose}
-    />
-  </Aux>
-);
+class Modal extends Component {
+  
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.show !== this.props.show;
+  }
+  
+  componentDidUpdate = () => {
+    console.log("Modal will Update")
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aux>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }} 
+        >
+          {this.props.children}
+        </div>
+        <Backdrop 
+          show={this.props.show}
+          clicked={this.props.modalClose}
+        />
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
