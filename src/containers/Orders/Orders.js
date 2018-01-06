26a1b3f75d757b08bb0b51c31624cf17077cc332
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-06 15:26:42 +0700

    Add componentDidMount() to get data from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandlers/withErrorHandlers';
 
 import Order from '../../components/Order/Order';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component{
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(resp => {
+        let fetchOrder = [];
+        for (let key in resp.data) {
+          fetchOrder.push({
+            ...resp.data[key],
+            id: key
+          });
+        }
+        this.setState({loading: false, orders: fetchOrder});
+      })
+      .catch(err => {
+        this.setState({loading: false});
+      })
+  }
+  
   render() {
     return (
       <div>
@@ -13,4 +38,4 @@ class Orders extends Component{
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
