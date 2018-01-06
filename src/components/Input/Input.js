9940d6478a65803ce2d75eca5a44d99182c25ac9
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-06 17:41:50 +0700

    Add css module to Input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
+import classes from './Input.css';
+
 const input = (props) => {
   switch(props.inputType) {
     case ('input'): 
-      inputElement = <input {...props}/>;
+      inputElement = <input className={classes.InputElement} {...props}/>;
       break;
     case ('textarea'):
-      inputElemnt = <textarea {...props}/>;
+      inputElemnt = <textarea className={classes.InputElement} {...props}/>;
       break;
     default: 
-      inputElement = <input {...props}/>;
+      inputElement = <input className={classes.InputElement} {...props}/>;
   }
   
-  return (<div>
-    <label htmlFor="">{props.label}</label>
+  return (<div className={classes.Input}>
+    <label htmlFor="" className={classes.Label}>{props.label}</label>
     {inputElement}
   </div>);
 };
