763dd935a64e95ae2af1e2eadab482852451b834
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-07 12:49:03 +0700

    Add touched properties in orderForm

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,7 +18,8 @@ class ContactData extends Component {
           value: '',
           validation: {
            required: true 
-          }
+        },
+          touched: false
         },
         street: {
           elementType: 'input',
@@ -30,7 +31,8 @@ class ContactData extends Component {
           validation: {
             required: true 
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         zipCode: {
           elementType: 'input',
@@ -44,7 +46,8 @@ class ContactData extends Component {
             minLength: 5,
             maxLength: 5
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         country: {
           elementType: 'input',
@@ -56,7 +59,8 @@ class ContactData extends Component {
           validation: {
             required: true 
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         email: {
           elementType: 'input',
@@ -68,7 +72,8 @@ class ContactData extends Component {
           validation: {
             required: true 
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         deliveryMethod: {
           elementType: 'select',
