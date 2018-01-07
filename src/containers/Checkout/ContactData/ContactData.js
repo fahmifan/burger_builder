babbc9d5965d694d05000fc431250001c4a21754
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-07 15:48:13 +0700

    zipcode value is string, change value.length

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -134,11 +134,11 @@ class ContactData extends Component {
     }
 
     if(rules.minLength) {
-      isValid = value.minLength >= rules.minLength && isValid;
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if(rules.maxLength) {
-      isValid = value.maxLength >= rules.maxLength && isValid;
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
     return isValid;
