d1e3e05d1570386e7c05ddeda6ac404862cc5cac
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 06:25:03 +0700

    assign <Provider></Provider> into const app

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+const app = (
   <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
   </Provider>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
