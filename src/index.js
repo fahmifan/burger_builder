f10e54cdade0a3ab9eae72102d856f38c94610a6
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 06:27:22 +0700

    group import based on dependecies and not

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
@@ -12,9 +13,9 @@ const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
