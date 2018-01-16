d4404bb1fb0c54cd81c1286beecdb20e6dc081c4
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 07:32:35 +0700

    create store from reducer.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
