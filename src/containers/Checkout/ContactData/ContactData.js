a7d3d6477546b1c5f4b98e215c9dab1f88c6be44
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-16 09:39:13 +0700

    import connect from react-redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from '../../../axios-order';
 
 import Button from '../../../components/UI/Button/Button';
