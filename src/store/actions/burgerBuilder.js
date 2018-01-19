bd372a968172edc333b9904b2900621fcf75c29e
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-19 09:54:01 +0700

    add setIngredient method and fetch ingredient method

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-order';
 
 export const addIngredient = name => {
   return {
@@ -13,3 +14,31 @@ export const removeIngredient = name => {
     ingredientName: name
   }
 };
+
+export const setIngredient = ingredients => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    ingredients: {
+      salad: ingredients.salad,
+      bacon: ingredients.bacon,
+      chesse: ingredients.chesse,
+      meat: ingredients.meat
+    }
+  }
+}
+
+export const fetchIngredientFailed = () => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED
+  }
+}
+
+export const initIngredient = dispatch => {
+  axios.get('https://react-myburger-19cb8.firebaseio.com/ingredients.json')
+    .then(res => {
+      dispatch(setIngredient(res.data))
+    })
+    .catch(error => {
+      dispatch(fetchIngredientFailed())
+    })
+}
