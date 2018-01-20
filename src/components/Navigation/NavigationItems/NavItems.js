890694d703a1540e8fbfdd740c1d2eebfcac7b56
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-20 17:00:27 +0700

    Add Authentication Nav

--- src/components/Navigation/NavigationItems/NavItems.js
+++ src/components/Navigation/NavigationItems/NavItems.js
@@ -7,6 +7,7 @@ const navItems = () => (
   <ul className={classes.NavItems}>
     <NavItem link="/" exact>Burger Builder</NavItem>
     <NavItem link="/orders">Orders</NavItem>
+    <NavItem link="/auth">Authenticate</NavItem>
   </ul>
 );
 
