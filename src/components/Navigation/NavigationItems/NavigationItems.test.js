5f5717e3a462e339b83ec4a038813449e7fbb5a2
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-23 21:22:53 +0700

    Add more test using another enzyme func

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,5 +21,10 @@ describe('<NavigationItems />', () => {
     wrapper.setProps({isAuth: true})
     expect(wrapper.find(NavItem)).toHaveLength(3);
   });
+
+  it('should render Log Out navigation element if authenticated', () => {
+    wrapper.setProps({isAuth: true})
+    expect(wrapper.contains(<NavItem link="/logout">Log Out</NavItem>)).toEqual(true);
+  });
 });
 
