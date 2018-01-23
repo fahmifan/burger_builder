e9ea0e247d243fd22554a1a078a0003c019ea03e
fahmi-irfan <fahmiirfan909@gmail.com>
2018-01-23 21:07:24 +0700

    Add more test and using enzyme helper

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,9 +8,18 @@ import NavItem from './NavigationItem/NavItem';
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it('should render two navigation element if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavItem)).toHaveLength(2);
+  });
+
+  it('should render three navigation element if authenticated', () => {
+    wrapper.setProps({isAuth: true})
+    expect(wrapper.find(NavItem)).toHaveLength(3);
   });
 });
 
